4aed2b6af4ec8f5c0bd724c9aefa98bf08906e7f
nischalstha <nischal@in10t.ag>
2020-06-27 15:13:05 -0500

    Updates

---
+++ precache-manifest.6e69f638ef121d78824a3cfb01ef36b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76be769b035717817f31ecdea45f3cfe",
+    "url": "/foodnetwork/index.html"
+  },
+  {
+    "revision": "128c796caccdd5830919",
+    "url": "/foodnetwork/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92b4b11bf000ff5bdca7",
+    "url": "/foodnetwork/static/js/2.fff40d0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodnetwork/static/js/2.fff40d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128c796caccdd5830919",
+    "url": "/foodnetwork/static/js/main.7b6f1e7f.chunk.js"
+  },
+  {
+    "revision": "41240ed716d8d7fb7cb6",
+    "url": "/foodnetwork/static/js/runtime-main.c78db3da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foodnetwork/static/media/logo.5d5d9eef.svg"
+  }
+]);
