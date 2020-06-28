3c3f1b606977dc9c3feb7e4032a732b75d9affee
nischalstha <nischal@in10t.ag>
2020-06-27 22:44:40 -0500

    Updates

--- precache-manifest.cbed98a632144836eed4214d1c645e8f.js
+++ precache-manifest.cbed98a632144836eed4214d1c645e8f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76be769b035717817f31ecdea45f3cfe",
+    "revision": "cf2012df580094e8653733cc79591fdb",
     "url": "/foodnetwork/index.html"
   },
   {
-    "revision": "128c796caccdd5830919",
-    "url": "/foodnetwork/static/css/main.5f361e03.chunk.css"
+    "revision": "bcbe3aec66594a5eb628",
+    "url": "/foodnetwork/static/css/main.523f09c5.chunk.css"
   },
   {
     "revision": "92b4b11bf000ff5bdca7",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/foodnetwork/static/js/2.fff40d0f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "128c796caccdd5830919",
-    "url": "/foodnetwork/static/js/main.7b6f1e7f.chunk.js"
+    "revision": "bcbe3aec66594a5eb628",
+    "url": "/foodnetwork/static/js/main.35879a8c.chunk.js"
   },
   {
     "revision": "41240ed716d8d7fb7cb6",
     "url": "/foodnetwork/static/js/runtime-main.c78db3da.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/foodnetwork/static/media/logo.5d5d9eef.svg"
+    "revision": "82074867e70b6dbe340875275f87e20e",
+    "url": "/foodnetwork/static/media/foodnetwork.82074867.png"
   }
 ]);
