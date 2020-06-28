b98391754a1eb51627565f2bf4af8d1a7634127d
nischalstha <nischal@in10t.ag>
2020-06-28 00:13:12 -0500

    c

--- src/App.js
+++ src/App.js
@@ -11,18 +11,10 @@ function App() {
         <li>Anish Karki</li>
         <li>Ashish Rajthala: Helloo I am goona be working on log in and sigh up.</li>
         <li>Badal Karki</li>
-<<<<<<< HEAD
         <li>Nischal Shrestha</li>
         </ul>
-=======
-        <li>Nischal Shrestha</ li>
-        <li>Food Recipe app</li>
-        </ul>
-<<<<<<< HEAD
         <p> Wassssssup Dawgs!!!</p>
-=======
->>>>>>> 43588808a416152658ccac87bba502105f8ccb97
->>>>>>> 04ba35a5d0d88914b26a61dec8b062d6deb5629d
+
       </header>
     </div>
   );
